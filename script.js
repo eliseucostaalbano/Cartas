3247f9653c76c615455586fce8fa46a111e1910b
Eliseu costa <eliseualbano57@gmail.com>
2024-12-06 16:57:56 -0300

    começando o processo dinamico

---
+++ script.js
@@ -0,0 +1,6 @@
+const objetosCartasDefinição = [
+    {id:1 , ImagePath :"Imagens/card-KingHearts.png"},
+    {id:2 , ImagePath :"Imagens/card-JackClubs.png"},
+    {id:3 , ImagePath :"Imagens/card-QueenDiamonds.png"},
+    {id:4 , ImagePath :"Imagens/card-AceSpades.png"}
+]
